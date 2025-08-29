a5119d9c49cba72769e12d67e315d1d167d4632a
Muhammad Hamza <123117450+muhammadhamza76@users.noreply.github.com>
2025-08-30 00:10:57 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio1-hamza/",  // ← Added base path
+  base: "/portfolio1-hamza",  // ← Added base path
   server: {
     host: "::",
     port: 8080,
